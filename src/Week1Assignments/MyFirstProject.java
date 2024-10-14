e9e76558a0b7cf7ab520c0acb3bd8984ebb6e878
Trent Lell <trentlell1981@gmail.com>
2024-10-14 15:09:06 -0600

    Git Push Practice

---
+++ src/Week1Assignments/MyFirstProject.java
@@ -0,0 +1,14 @@
+package Week1Assignments;
+
+public class MyFirstProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("My first Java Program!");
+
+
+	}
+
+}
